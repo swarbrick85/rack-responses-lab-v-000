6eb4773fdd97200fb9507d2357e3935610b825e8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-28 21:53:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     if hour < 12 
-      resp.write 
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end 
     resp.finish
   end
  
